5f5b0de122be50b9d3571e84035982be4636b812
Ivan Gutev <gutev.ivan@gmail.com>
2016-04-05 03:40:06 +0100

    fixing tags

--- build_db/02-rds.tf
+++ build_db/02-rds.tf
@@ -3,7 +3,7 @@
 #################################################
 # DB Parameter Group
 resource "aws_db_parameter_group" "default" {
-  name  = "${var.tag_service}-${var.tag_environment}-db-${var.db_instance_count}-pg"
+  name  = "${var.tag_service}-db-${var.db_instance_count}-pg"
   family = "mysql5.6"
   description = "${var.tag_project}-${var.tag_environment} DB parameter group"
 
@@ -13,7 +13,7 @@ resource "aws_db_parameter_group" "default" {
   }
 
   tags {
-    Name        = "${var.tag_service}-${var.tag_project}-${var.tag_environment}-db-pg"
+    Name        = "${var.tag_service}-${var.tag_environment}-db-pg"
     Build       = "Automatic"
     Creator     = "${var.tag_creator}"
     Department  = "${var.tag_department}"
