35c76dc54a14f742926f7c42d29ed6cde15ed76d
Chris Greatbanks <cgreatbanks@gmail.com>
2016-04-06 14:47:22 +0100

    Fix various variable defaults

--- payments/terraform/variables.tf
+++ payments/terraform/variables.tf
@@ -1,8 +1,12 @@
 # General
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_key_path" {}
-variable "aws_key_name" {}
+variable "aws_key_path" {
+  default = "~/.ssh/eurostar.pem"
+}
+variable "aws_key_name" {
+  default = "eurostar"
+}
 
 variable "region" {
     description = "EC2 Region for the VPC"
@@ -78,7 +82,6 @@ variable "asg_desired" {
 variable "tag_environment" {}
 variable "tag_project" {}
 variable "tag_service" {}
-variable "tag_role" {}
 variable "tag_creator" {}
 
 # REQUIRED FOR PEERING TO ADMIN VPC - START
@@ -122,7 +125,9 @@ variable "route-table-adminvpn-unknown3" {
 
 #var.tag_servicecriticality {}
 #var.tag_supportcontact {}
-variable "tag_department" {}
+variable "tag_department" {
+  default = "Digital Systems"
+}
 variable "tag_environment" {}
 
 # REQUIRED FOR PEERING TO ELK VPC - START
@@ -144,7 +149,9 @@ variable "route-table-elk" {
   default = "rtb-d66974b3"
 }
 
-variable "domain_name_servers" {}
+variable "domain_name_servers" {
+  default = "AmazonProvidedDNS"
+}
 
 
 # REQUIRED FOR PEERING TO ELK VPC - END
