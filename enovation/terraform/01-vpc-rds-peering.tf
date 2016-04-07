7199353d280b16128012aff57f0c1289d1c9ac6d
Chris Greatbanks <cgreatbanks@gmail.com>
2016-04-07 12:47:13 +0100

    Use variables in db peering

--- enovation/terraform/01-vpc-rds-peering.tf
+++ enovation/terraform/01-vpc-rds-peering.tf
@@ -1,18 +1,18 @@
 resource "aws_vpc_peering_connection" "db" {
-  peer_owner_id = "018274991670"
-  peer_vpc_id = "vpc-5b49433e"
+  peer_owner_id = "${var.peer_owner_id}"
+  peer_vpc_id = "${var.db_vpc_id}"
   vpc_id = "${aws_vpc.default.id}"
   auto_accept = true
 }
 
 resource "aws_route" "peering-route-1a" {
-  route_table_id = "rtb-e037ce84"
+  route_table_id = "${var.db_route_table_id}"
   destination_cidr_block = "${var.private_subnet_cidr_1a}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.db.id}"
 }
 
 resource "aws_route" "peering-route-1b" {
-  route_table_id = "rtb-e037ce84"
+  route_table_id = "${var.db_route_table_id}"
   destination_cidr_block = "${var.private_subnet_cidr_1b}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.db.id}"
 }
