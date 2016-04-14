0e0fceb05c2eab647cdb55c16e2e3e938e211696
Chris Greatbanks <cgreatbanks@gmail.com>
2016-04-14 16:14:20 +0100

    Fix peering to notification, booking and payments

--- enovation/terraform/06-payments-peering.tf
+++ enovation/terraform/06-payments-peering.tf
@@ -1,43 +1,31 @@
-# Peering with payments VPC
-# Required to allow access on VPN to Private IP/DNS
-# - https://www.terraform.io/docs/providers/aws/r/vpc_peering.html
-
-variable "peer_vpc_id_payments" {
-  description = "Peer Microservice VPC with payments-VPC ID"
-}
-
-variable "cidr-payments" {
-  description = "CIDR for payments-routetable"
-}
-
-variable "route-table-payments" {
-  description = "Route Table for payments VPC"
-}
-
-variable "domain_name_servers" {
-  default = "AmazonProvidedDNS"
-}
-
+variable "payments_vpc_id" {}
+variable "payments_vpc_cidr" {}
+variable "payments_route_table_id_1a" {}
+variable "payments_route_table_id_1b" {}
 
 resource "aws_vpc_peering_connection" "payments" {
   peer_owner_id = "${var.peer_owner_id}"
-  peer_vpc_id = "${var.peer_vpc_id_payments}"
+  peer_vpc_id = "${var.payments_vpc_id}"
   vpc_id = "${aws_vpc.default.id}"
-  # Accept the peering (you need to be the owner of both VPCs)
   auto_accept = true
-
-  tags {
-    Name        = "${var.tag_project}-peering-to-payments"
-    Environment = "${var.tag_environment}"
-    Project     = "${var.tag_project}"
-    Service     = "${var.tag_service}"
-    Creator     = "${var.tag_creator}"
-    Role        = "Network"
-  }
 }
 
-resource "aws_route" "payments" {
-  route_table_id = "${var.route-table-payments}"
+# Add current VPC route into Notification Route Tables
+resource "aws_route" "peering-route-payments1a-to-vpc" {
+  route_table_id = "${var.payments_route_table_id_1a}"
   destination_cidr_block = "${var.vpc_cidr}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.payments.id}"
+}
+
+resource "aws_route" "peering-route-notifivation1b-to-vpc" {
+  route_table_id = "${var.payments_route_table_id_1b}"
+  destination_cidr_block = "${var.vpc_cidr}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.payments.id}"
+}
+
+# Add Notification route to current VPCs Private route table
+resource "aws_route" "payments-peering-private" {
+  route_table_id = "${aws_route_table.private.id}"
+  destination_cidr_block = "${var.payments_vpc_cidr}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.payments.id}"
 }
