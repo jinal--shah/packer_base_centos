3aa1d37f54d3b6b18748467ba49ebc94a72fae23
Chris Greatbanks <cgreatbanks@gmail.com>
2016-04-14 16:45:52 +0100

    single route table to update for booking and payments. Associated with multi subnets

--- enovation/terraform/06-payments-peering.tf
+++ enovation/terraform/06-payments-peering.tf
@@ -1,7 +1,6 @@
 variable "payments_vpc_id" {}
 variable "payments_vpc_cidr" {}
-variable "payments_route_table_id_1a" {}
-variable "payments_route_table_id_1b" {}
+variable "payments_route_table_id" {}
 
 resource "aws_vpc_peering_connection" "payments" {
   peer_owner_id = "${var.peer_owner_id}"
@@ -11,14 +10,8 @@ resource "aws_vpc_peering_connection" "payments" {
 }
 
 # Add current VPC route into Notification Route Tables
-resource "aws_route" "peering-route-payments1a-to-vpc" {
-  route_table_id = "${var.payments_route_table_id_1a}"
-  destination_cidr_block = "${var.vpc_cidr}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.payments.id}"
-}
-
-resource "aws_route" "peering-route-notifivation1b-to-vpc" {
-  route_table_id = "${var.payments_route_table_id_1b}"
+resource "aws_route" "peering-route-payments-to-vpc" {
+  route_table_id = "${var.payments_route_table_id}"
   destination_cidr_block = "${var.vpc_cidr}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.payments.id}"
 }
