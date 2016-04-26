f083218019f275af40dca7b30bb52b8cc6c710d7
Chris Greatbanks <cgreatbanks@gmail.com>
2016-04-26 12:11:31 +0100

    Add tf s3 remote state

---
+++ booking/terraform/remotestate.tf
@@ -0,0 +1,9 @@
+# TFState to S3
+resource "terraform_remote_state" "tfstate" {
+  backend = "s3"
+  config {
+    bucket = "eurostar-terraform-state-${var.tag_environment}"
+    key = "${var.tag_service}/vpc-rds.tfstate"
+    region = "eu-west-1"
+  }
+}
