3e8dd8c02b1631b0f0f525945cd2418efe1f541a
jinal--shah <jin jnshah@gmail.com>
2016-04-27 14:03:48 +0000

    split out cob stuff in to separate source bundle

--- packer/scripts/add_metrics_client_config.sh
+++ packer/scripts/add_metrics_client_config.sh
@@ -21,15 +21,18 @@ METRICS_RPMS="
     statsite
     collectd-rrdtool
 "
-   tar -C /tmp -xzf /tmp/cob_bundle.tgz       \
-&& tar -C /tmp -xzf /tmp/emon_bundle.tgz      \
-&& cp -r /tmp/cob/* /                         \
-&& yum -y install yum-utils cloud-init        \
-&& yum-config-manager --enable eurostar_prod  \
-&& yum -y install $METRICS_RPMS               \
-&& mkdir -p /etc/collectd.d                   \
-&& cp -r /tmp/emon/* /                        \
-&& yum-config-manager --disable eurostar_prod \
-&& rm -rf /tmp/{cob,emon}                     \
+YUM_EMON_CONF=etc/yum.repos.d/emon.repo
+
+   tar -C /tmp -xzf /tmp/cob_bundle.tgz        \
+&& tar -C /tmp -xzf /tmp/emon_bundle.tgz       \
+&& cp -r /tmp/cob/* /                          \
+&& cp /tmp/emon/$YUM_EMON_CONF /$YUM_EMON_CONF \
+&& yum -y install yum-utils cloud-init         \
+&& yum-config-manager --enable eurostar_prod   \
+&& yum -y install $METRICS_RPMS                \
+&& mkdir -p /etc/collectd.d                    \
+&& cp -r /tmp/emon/* /                         \
+&& yum-config-manager --disable eurostar_prod  \
+&& rm -rf /tmp/{cob,emon}                      \
           /tmp/{cob,emon}_bundle.tgz
 
