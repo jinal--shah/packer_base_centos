d27f1ec7cba929d3e22bc4f1fc7a19e9dccc560b
Chris Greatbanks <cgreatbanks@gmail.com>
2016-05-04 12:17:42 +0100

    Add missing variable entries and error checking on alert logic install

--- packer/scripts/install_alertlogic.sh
+++ packer/scripts/install_alertlogic.sh
@@ -2,8 +2,36 @@
 # vim: et sr sw=4 ts=4 smartindent:
 # install_alertlogic.sh
 
-if [ -f /etc/eurostar/eurostar_service_info ]; then
-  source /etc/eurostar/eurostar_service_info
+REQUIRED_VARS="
+    ALERTLOGIC_HOST
+    ALERTLOGIC_KEY
+"
+
+function check_var_defined() {
+    var_name="$1"
+    var_val="${!var_name}"
+    if [[ -z $var_val ]]; then
+        echo "$0 ERROR: You must pass \$$var_name to this script" >&2
+        FAILED_VALIDATION="you bet'cha"
+        return 1
+    fi
+}
+
+function add_to_info_str() {
+    var_name="$1"
+    var_val="${!var_name}"
+    key_val="$var_name=$var_val"
+    OUTPUT_STR="$OUTPUT_STR\n$key_val"
+}
+
+for this_var in $REQUIRED_VARS; do
+    check_var_defined $this_var \
+    && add_to_info_str $this_var
+done
+
+if [[ ! -z $FAILED_VALIDATION ]]; then
+    echo "$0 EXIT: FAILURE. One of more required vars not passed to this script."
+    exit 1
 fi
 
 if [ ! -f "/etc/init.d/al-agent" ]; then
@@ -12,8 +40,8 @@ if [ ! -f "/etc/init.d/al-agent" ]; then
 fi
 
 # configure then provison the agent
+/etc/init.d/al-agent provision --key ${ALERTLOGIC_KEY} --inst-type role
 /etc/init.d/al-agent configure --host ${ALERTLOGIC_HOST}
-/etc/init.d/al-agent provision --key ${ALERTLOGIC_KEY} --inst-type role >/dev/null 2>&1
 if [ -f /var/alertlogic/etc/host_crt.pem ] && [ -f /var/alertlogic/etc/host_key.pem ];
   then echo "$0 INFO: Alert Logic agent keys generated successfully" >&2
   else echo "$0 ERROR: Alert Logic agent keys failed to generate: /var/alertlogic/etc/host_*.pem" >&2
