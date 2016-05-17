2d92fe4d1361d7b627ebeb6a2f5919939ded4a9a
jinal--shah <jnshah@gmail.com>
2016-05-17 10:01:56 +0000

    removed unneeded assets, rebased remaining ones to root repo folder

--- enovation/terraform/remotestate.tf
+++
@@ -1,9 +0,0 @@
-# TFState to S3
-resource "terraform_remote_state" "tfstate" {
-  backend = "s3"
-  config {
-    bucket = "eurostar-terraform-state-${var.tag_environment}"
-    key = "${var.tag_service}/vpc.tfstate"
-    region = "eu-west-1"
-  }
-}
