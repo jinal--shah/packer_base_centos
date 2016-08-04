887757a5034a95681309ad4d0f1c57e0f951a300
jinal--shah <jnshah@gmail.com>
2016-08-04 11:51:08 +0000

    added credstash and jq to eurostar_aws

---
+++ scripts/00030-install_credstash.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# vim: et sr sw=4 ts=4 smartindent:
+#
+# install_credstash.sh
+#
+# Installs:
+#
+# * python
+# * pip (yum package)
+# * credstash
+#
+# needs to use epel repo
+
+# Check for epel repo. Install if needed. Enable it.
+# Remember original state to disable epel
+
+echo "$0 INFO: ... installing credstash cli"
+
+echo "$0 INFO: ... checking for epel. Installing if needed."
+EPEL_RPM_URI=https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+EPEL_REPO_FILE=/etc/yum.repos.d/epel.repo
+
+if ! yum repolist all | awk {'print $1'} | grep '^epel$' 2>/dev/null
+then
+    echo "$0 ERROR: epel not installed. Can't continue." >&2
+    exit 1
+else
+    echo "$0 INFO: ... Found epel. Will enable only for this installation."
+fi
+
+yum -y install python python-pip --enablerepo epel \
+&& pip --no-cache-dir install --upgrade credstash
+
+if ! credstash -h >/dev/null 2>&1
+then
+    echo "$0 ERROR: credstash client not installed in to path correctly." >&2
+    echo "          ... See ERROR messages above." >&2
+    exit 1
+fi
+exit 0
---
+++ scripts/00030-install_jq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# vim: et sr sw=4 ts=4 smartindent:
+#
+# install_jq.sh
+#
+# Installs:
+#
+# * wget (to fetch jq)
+# * jq
+#
+
+URL_JQ=https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+BIN_PATH=/usr/local/bin/jq 
+
+yum -y install wget \
+&& wget $BIN_PATH -O "$URL_JQ" \
+&& chmod a+x $BIN_PATH
+
+if ! jq --version >/dev/null 2>&1
+then
+    echo "$0 ERROR: jq not installed in to path correctly." >&2
+    echo "          ... See ERROR messages above." >&2
+    exit 1
+fi
